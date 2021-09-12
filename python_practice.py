d778bacc2c1b0a1f191f7eba70f9ca146e9d2f07
gram89 <89284806+gram89@users.noreply.github.com>
2021-09-11 23:39:00 -0400

    Update python_practice.py

--- python_practice.py
+++ python_practice.py
@@ -1,3 +1,32 @@
-print ("Hello World")
+#print ("Hello World")
 #getting datatypes
-type(3)
+#input data
+#my_votes = int(input("How many votes did you get in the election?"))
+#total_votes = int(input("What are the total votes in the election?"))
+#percentage_votes = (my_votes/total_votes)*100
+#print (f"I recieved {percentage_votes}% of the total votes")
+
+#adding counties list
+# counties = ["Arapahoe", "Denver", "Jefferson"]
+# if counties[1] == "Denver":
+#     print(counties[1])
+# if counties[3] !="Jefferson":
+#     print(counties[2])
+
+# temperature = int(input("What is the temperature outside?"))
+# if temperature >80:
+#     print("Turn on the AC")
+# else:
+#     print("Open the windows")
+# if "Arapahoe" in counties:
+#     print("True")
+# else:
+#     print("False")
+# x = 0
+# while x<=5:
+#     print(x)
+#     x=x+1
+
+counties = ["Arapahoe", "Denver", "Jefferson"]
+for county in counties:
+    print(county)
